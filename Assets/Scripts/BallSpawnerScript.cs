bda14c96ae74ab96dd28a1b7030a7544c4b17972
bobemv <bobemv@gmail.com>
2016-09-24 17:10:04 +0200

    Faltaban archivos.

---
+++ Assets/Scripts/BallSpawnerScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class BallSpawnerScript : NetworkBehaviour {
+
+	[Header("Ball Gameobject")]
+	public GameObject ballPrefab;
+
+	public override void OnStartServer(){
+		GameObject ball;
+		Vector3 original = new Vector3 (0, 0, 0);
+		ball = (GameObject)Instantiate (ballPrefab, original, Quaternion.identity);
+		NetworkServer.Spawn (ball);
+	}
+
+	void Start(){
+	}
+
+	void Update(){
+	}
+}
