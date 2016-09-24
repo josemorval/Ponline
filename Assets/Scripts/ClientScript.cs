851913255094c72924e39371ce7e5311bc2f7691
bobemv <bobemv@gmail.com>
2016-09-25 01:49:59 +0200

    Implementación a medias usando Network.SimpleServer y Network.Client. Desechada (no encuentro la manera de hacer un server asíncronamente del todo dentro de Unity).

---
+++ Assets/Scripts/ClientScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class ClientScript : MonoBehaviour {
+
+	#region public variables
+	[Header("Datos del host")]
+	public string hostIpAddress;
+	public int hostPort;
+	#endregion
+
+	#region private variables
+	NetworkClient myClient;
+	short counterMessagges;
+
+	#endregion
+	/*
+	public void OnConnected(NetworkConnection conn, NetworkReader reader){
+		Debug.Log ("Connected to server");
+	}
+
+	public void OnDisconnected(NetworkConnection conn, NetworkReader reader){
+		Debug.Log ("Disonnected to server");
+	}
+
+	public void OnError(NetworkConnection conn, NetworkReader reader){
+		Debug.Log ("Error connecting");
+	}*/
+
+	// Custom initialization (has to be called in GameManager!!)
+	public void StartComponent (string hostIpAddress, int hostPort) {
+		myClient = new NetworkClient ();
+		this.hostIpAddress = hostIpAddress;
+		this.hostPort = hostPort;
+		counterMessagges = 0;
+		/*
+		myClient.RegisterHandler (MsgType.Connect, OnConnected);
+		myClient.RegisterHandler (MsgType.Disconnect, OnDisconnected);
+		myClient.RegisterHandler (MsgType.Error, OnError);
+		*/
+
+		myClient.Connect (hostIpAddress, hostPort);
+		/*Hacemos una especia de handshake para confirmar la conexion entre los dos*/
+		myClient.Send (counterMessagges, );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/GameManagerController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerController : MonoBehaviour {
+
+	#region public variables
+		[Header("GameObjects de la network")]
+		public GameObject server;
+		public GameObject client;
+
+		[Header("Botones para iniciar la network")]
+		public KeyCode serverKey;
+		public KeyCode clientKey;
+
+		[Header("Boton para salir de la network")]
+		public KeyCode exitKey;
+
+		[Header("Parámetros conexión")]
+		public string ipAddress;
+		public int serverPort;
+	#endregion
+
+
+	#region private variables
+
+		bool flag; //Para  detectar si ya se esta corriendo como host o cliente
+		GameObject networkManager;
+
+	#endregion
+
+	// Use this for initialization
+	void Start () {
+		flag = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Vemos si debemos instanciar algun network gameobject
+		if (!flag) {
+			if (Input.GetKey (serverKey)) {
+				networkManager = Instantiate (server);
+				networkManager.name = networkManager.name.Replace ("(Clone)", "");
+				flag = true;
+			} else if (Input.GetKey (clientKey)) {
+				networkManager = Instantiate (client);
+				networkManager.name = networkManager.name.Replace ("(Clone)", "");
+
+				networkManager.GetComponent<ClientScript> ().StartComponent (ipAddress, serverPort);
+
+				flag = true;
+			}
+		}
+
+		//Salir del multijugador
+		if (Input.GetKey (exitKey) && flag) {
+			Destroy (networkManager);
+			flag = false;
+		}
+
+
+
+	}
+}
